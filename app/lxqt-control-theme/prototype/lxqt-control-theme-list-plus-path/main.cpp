797b39a82cac7183a6260936e58f65bc21606390
samwhelp <samwhelp@users.noreply.github.com>
2018-10-13 00:19:42 +0800

    lxqt-control-theme-list-path

---
+++ app/lxqt-control-theme/prototype/lxqt-control-theme-list-plus-path/main.cpp
@@ -0,0 +1,22 @@
+
+
+#include <QTextStream>
+
+
+#include <LXQt/Settings>
+
+
+int main (int argc, char **argv)
+{
+	// http://doc.qt.io/qt-5/qtextstream.html#details
+	QTextStream out(stdout);
+
+	const QList<LXQt::LXQtTheme> themes = LXQt::LXQtTheme::allThemes();
+
+	foreach(const LXQt::LXQtTheme &theme, themes) {
+		out << theme.name() << " : " << theme.path() << endl;
+	}
+
+	return 0;
+
+}
